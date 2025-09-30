879758c99239595c04d0c2ffca7c7b2d2ea3e230
DonovanJ204 <donovan.jeannot.stcharles@gmail.com>
2025-09-30 08:12:20 +0000

    feat: Exercices sur les fondamentaux JS terminés

---
+++ script.js
@@ -0,0 +1,21 @@
+const nom = "Donovan";
+let age = 18;
+console.log(nom,age)
+
+if (age < 13) {
+    console.log("Accès interdit aux moins de 13 ans.");
+} else if (age < 18) {
+    console.log("Vous pouvez accéder à la section ado.");
+} else {
+    console.log("Bienvenue dans l'espace adulte.");
+}
+
+for (let i = 0; i < 11; i++) {
+    console.log("Cal: " + 7*[i]);
+}
+
+function saluer(prenom) {
+    return "Bonjour " + prenom + " ! Comment vas-tu ?";
+}
+
+console.log(saluer("Jeannot"));
